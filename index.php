d42e887cb9088ce0db201fcf0a6f86b4b2be0786
Evan Gipson <evangipson@gmail.com>
2017-10-09 02:40:49 -0500

    Break directory crawl into it's own file

--- index.php
+++ index.php
@@ -8,70 +8,7 @@
     <link rel="stylesheet" type="text/css" href="./style.css?00014" media="screen" />
   </head>
   <body>
-    <?
-        $albumList =  array();
-        $songTiles = array();
-        function dirToOptions($path = __DIR__, $level = 0) {
-            global $albumList;
-            global $songTiles;
-            $albumArtPicture = false;
-            $fullPath = "";
-            $items = scandir($path);
-            // First try and find album art.
-            foreach($items as $item) {
-                $fullPath = $path . DIRECTORY_SEPARATOR . $item;
-                if(is_file($fullPath)) {
-                    // Handle album art
-                    if(strpos($item, ".jpg") !== false || strpos($item, ".jpeg") !== false || strpos($item, ".png") !== false || strpos($item, ".bmp") !== false) {
-                        $albumArtPicture = array_splice(explode("/", $fullPath), 5);
-                        $albumArtPicture = implode("/", $albumArtPicture);
-                    }
-                }
-            }
-            // Take each item in our directory scan and handle it.
-            foreach($items as $item) {
-                // Ignore items starting with a dot (= hidden or nav)
-                if (strpos($item, '.') === 0) {
-                    continue;
-                }
-                $fullPath = $path . DIRECTORY_SEPARATOR . $item;
-                // Create our nice "local" path for javascript
-                $localPath = array_splice(explode("/", $fullPath), 5);
-                $localPath = implode("/", $localPath);
-                // and a "friendly" song name
-                $songName = array_splice(explode(".", $item), 0)[0];
-                // Get timestamp from album
-                $stat = stat($fullPath);
-                // Handle what we find.
-                if(is_file($fullPath)) {
-                    $album = substr($path, strrpos($path, '/') + 1);
-                    // Handle music files
-                    if(strpos($item, ".mp3") !== false || strpos($item, ".wav") !== false) {
-                        $songTiles[] = "<div class='song' ".($albumArtPicture !== false ? "data-album-art='".$albumArtPicture."'" : "")." data-song-path='".$localPath."' data-album='".$album."' data-song='".$songName."'>
-                            <h2>$songName</h2>
-                            <p>$album</p>
-                            <p>".date("Y",$stat['mtime'])."</p>
-                            <div class='song-play-button'>
-                                <i class='material-icons'>play_circle_outline</i>
-                            </div>
-                        </div>";
-                    }
-                }
-                else if (is_dir($fullPath)) {
-                    $album = substr($fullPath, strrpos($fullPath, '/') + 1);
-                    if(in_array($album, $albumList) == false) {
-                        $albumList[] = $album;
-                    }
-                    // recursive call to self to add the subitems
-                    dirToOptions($fullPath, $level + 1);
-                }
-            }
-        }
-        dirToOptions();
-        // Randomize albums and songs before display.
-        shuffle($albumList);
-        shuffle($songTiles);
-    ?>
+    <?php include "./directory-crawl.php"; ?>
     <nav id="albumList">
         <input type="checkbox" id="menu">
         <label for="menu" class="mobile-menu-toggle"><i class="material-icons">menu</i></label>
