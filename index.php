3d9b057dee62af28763dddece551902b0fa0c20b
Evan Gipson <evangipson@gmail.com>
2017-10-09 02:19:47 -0500

    Add border style to IE11 warning

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Evan Gipson's Music | Powered by LiMP</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" type="text/css" href="./style.css?00012" media="screen" />
+    <link rel="stylesheet" type="text/css" href="./style.css?00013" media="screen" />
   </head>
   <body>
     <?
@@ -91,7 +91,7 @@
         Your browser does not support the audio format.
     </audio>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="./brain.js?00013"></script>
+    <script src="./brain.js?00014"></script>
     <div id="browserWarning">
         <p>Sorry!</p>
         <p>Your browser isn't supported! Try using <a href="https://www.mozilla.org/en-US/firefox/new/">Firefox</a>, <a href="https://www.google.com/chrome/browser/desktop/index.html">Chrome</a>, <a href="https://www.microsoft.com/en-us/windows/microsoft-edge">Edge</a>, or <a href="https://www.apple.com/safari/">Safari</a>.</p>
